2752f37083ed588c52d48c23159ecf626a8746d6
Greg P <gorgarp@gmail.com>
2023-01-21 15:32:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
+from datetime import datetime
 
 # list of exchange we want to include
 exchanges = ["Coinbase","Kucoin","Tradeogre"]
 
-# Load historical data from file
-try:
-    df = pd.read_csv("historical_data.csv")
-except FileNotFoundError:
-    # If file doesn't exist, download data from CoinMarketCap API
-    endpoint = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical"
-    params = {
-        "interval": "1d",
-        "start": "2022-01-01",
-        "end": "2022-01-31",
-        "convert": "USD"
-    }
-    headers = {
-        "X-CMC_PRO_API_KEY": "YOUR_API_KEY"
-    }
-    response = requests.get(endpoint, headers=headers, params=params)
-    data = json.loads(response.text)
-    df = pd.DataFrame(data["data"])
-    df = df[["name", "quote.USD.close", "quote.USD.volume_24h","market_data.exchanges"]]
-    df = df[df["market_data.exchanges"].apply(lambda x: any(i['name'] in exchanges for i in x))]
-    df.columns = ["name", "close", "volume","exchanges"]
-    df.to_csv("historical_data.csv", index=False)
+# download all historical data from CoinMarketCap API
+endpoint = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical"
+params = {
+    "interval": "1d",
+    "convert": "USD"
+}
+headers = {
+    "X-CMC_PRO_API_KEY": "YOUR_API_KEY"
+}
+response = requests.get(endpoint, headers=headers, params=params)
+data = json.loads(response.text)
+df = pd.DataFrame(data["data"])
+df = df[["name", "quote.USD.close", "quote.USD.volume_24h","market_data.exchanges"]]
+df = df[df["market_data.exchanges"].apply(lambda x: any(i['name'] in exchanges for i in x))]
+df.columns = ["name", "close", "volume","exchanges"]
+
+# save the data to file
+df.to_csv("historical_data.csv", index=False)
 
 # Load previous predictions from file
 try:
@@ -41,20 +36,34 @@ for coin in df["name"].unique():
     coin_df = df[df["name"] == coin]
 
     # Create the X and y arrays for the model
-    X = coin_df[["volume"]].values
-    y = coin_df["close"].values
+    X = coin_df[["close", "volume"]].values
+    y = coin_df["close"].pct_change().shift(-1).fillna(0)
 
-    # Create the model
-    model = RandomForestRegressor().fit(X, y)
+    # Split the data into training and testing sets
+    split_index = int(coin_df.shape[0] * 0.8)
+    X_train, X_test = X[:split_index], X[split_index:]
+    y_train, y_test = y[:split_index], y[split_index:]
 
-    # Make predictions for next 7 days
-    X_test = coin_df.tail(7)["volume"].values
-    predictions = model.predict(X_test.reshape(-1, 1))
-    predicted_return = (predictions[-1] / predictions[0]) - 1
+    # Train the model
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
 
-    # Check if coin is already in predictions DataFrame
-    coin_predictions_df = predictions_df[predictions_df["coin"] == coin]
-    if coin_predictions_df.empty:
-        # Coin is not in predictions DataFrame, so add it
-        actual_return = None
-        predictions_df = predictions_df.append({"coin": coin, "predicted_return": predicted_
+       # Make predictions
+    y_pred = model.predict(X_test)
+
+    # Calculate the accuracy of the model
+    coin_predictions_df = pd.DataFrame({"coin": coin,
+                                       "predicted_return": y_pred,
+                                       "actual_return": y_test})
+    predictions_df = predictions_df.append(coin_predictions_df, ignore_index=True)
+
+    # Check if any predictions have a return greater than 1000%
+    predictions_df["predicted_return"] = predictions_df["predicted_return"] * 100
+    predictions_df["actual_return"] = predictions_df["actual_return"] * 100
+    predictions_df = predictions_df[predictions_df["predicted_return"] > 1000]
+
+    # Save predictions to file
+    predictions_df.to_csv("predictions.csv", index=False)
+
+# Print the number of predictions
+print(f"{len(predictions_df)} predictions made.")
