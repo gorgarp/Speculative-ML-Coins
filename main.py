a36081c7dfd7fc7fc4099fa9f46b2de18967d8d7
Greg P <gorgarp@gmail.com>
2023-01-21 15:54:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from datetime import datetime
 import requests
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
-# list of exchange we want to include
 exchanges = ["Coinbase","Kucoin","Tradeogre"]
-
-# download all historical data from CoinMarketCap API
-endpoint = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
-params = {
-    "start": "1",
-    "convert": "USD"
-}
 headers = {
     "X-CMC_PRO_API_KEY": "YOUR_API_KEY"
 }
-response = requests.get(endpoint, headers=headers, params=params)
-data = response.json()
+params = {
+    "convert":"USD"
+}
+
+# download all historical data from CoinMarketCap API
+response = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", headers=headers, params=params)
+data = response.json()["data"]
 
 # Extract the data
-df = pd.DataFrame(data["data"])
-df = df[["name", "quote.USD.price", "quote.USD.volume_24h","exchanges"]]
-df = df[df["exchanges"].apply(lambda x: any(i['name'] in exchanges for i in x))]
-df.columns = ["name", "price", "volume","exchanges"]
+df = pd.DataFrame(data)
+df = df[["name", "symbol", "quotes.USD.price", "quotes.USD.volume_24h"]]
+df.columns = ["name", "symbol", "price", "volume"]
+
+# filter the data to include only the coins listed on the specified exchanges
+response = requests.get("https://pro-api.coinmarketcap.com/v1/exchange/listings/latest", headers=headers)
+data = response.json()["data"]
+exchanges_df = pd.DataFrame(data)
+exchanges_df = exchanges_df[["name","slug"]]
+exchanges_df = exchanges_df[exchanges_df["name"].isin(exchanges)]
+exchanges_df = exchanges_df.rename(columns={'slug':'symbol'})
+df = pd.merge(df, exchanges_df, on='symbol')
 
 # save the data to file
 df.to_csv("historical_data.csv", index=False)
@@ -58,14 +63,10 @@ for coin in df["name"].unique():
                                        "actual_return": y_test})
     predictions_df = predictions_df.append(coin_predictions_df, ignore_index=True)
 
-
     # Check if any predictions have a return greater than 1000%
-    predictions_df["predicted_return"] = predictions_df["predicted_return"] * 100
-    predictions_df["actual_return"] = predictions_df["actual_return"] * 100
-    predictions_df = predictions_df[predictions_df["predicted_return"] > 1000]
+    predictions_df["predicted_return"] = predictions_df["predicted_return"].apply(lambda x: x / y_test.mean() - 1)
+    predictions_df["actual_return"] = predictions_df["actual_return"].apply(lambda x: x / y_test.mean() - 1)
+    predictions_df = predictions_df[predictions_df["predicted_return"] > 0.1]
 
     # Save predictions to file
     predictions_df.to_csv("predictions.csv", index=False)
-
-# Print the number of predictions
-print(f"{len(predictions_df)} predictions made.")
