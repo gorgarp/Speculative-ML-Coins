c3c946c6640fdca2d9b9942ca1e9481fbb1ea0c3
Greg P <gorgarp@gmail.com>
2023-01-21 16:42:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
 import json
 import pandas as pd
+import os
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
 
 # Function to download historical price data for all coins
 def download_data():
@@ -31,39 +30,35 @@ def download_data():
             # Extract historical price data
             df = pd.read_json(json.dumps(data['result']['86400']))
             df.columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volume_quote']
-            # Save to file
+             # Save to file
             df.to_csv(f"{symbol}_prices.csv")
         else:
             print(f"Coin {symbol} doesn't have trading pair in the exchange.")
-
-
+    return coins
 # Function to persist data between runs
-def persist_data():
-    # Load data from files and store in a variable
+def persist_data(coins):
     data = {}
     for coin in coins:
         symbol = coin["symbol"]
-        df = pd.read_csv(f"{symbol}_prices.csv")
-        data[symbol] = df
+        # check if the coin have trading pair in the exchange
+        if 'exchange' in coin:
+            if os.path.exists(f"{symbol}_prices.csv"):
+                df = pd.read_csv(f"{symbol}_prices.csv")
+                data[symbol] = df
     return data
 
 # Function to download latest data each run
 def update_data(data):
-    # Loop through each coin to download latest data
-    for coin in coins:
-        symbol = coin["symbol"]
-        url = f"https://api.cryptowat.ch/markets/{symbol}/ohlc"
+    for symbol in data.keys():
+        df = data[symbol]
+        last_timestamp = df['timestamp'].iloc[-1]
+        url = f"https://api.cryptowat.ch/markets/{exchange}/{trading_pair}/ohlc?after={last_timestamp}"
         response = requests.get(url)
         new_data = json.loads(response.text)
-        
-        # Extract latest price data
-        prices = new_data["result"]["86400"]
-        
-        # Convert to pandas DataFrame
-        new_df = pd.DataFrame(prices, columns=["timestamp", "open", "high", "low", "close", "volume", "volume_quote"])
-        
-        # Append new data to existing data
-        data[symbol] = data[symbol].append(new_df)
+        new_df = pd.read_json(json.dumps(new_data['result']['86400']))
+        new_df.columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volume_quote']
+        df = df.append(new_df)
+        df.to_csv(f"{symbol}_prices.csv", index=False)
     return data
 
 # Function to use machine learning to predict next day's closing price
@@ -72,46 +67,46 @@ def predict_price(df):
     X = df[['open', 'high', 'low', 'volume', 'volume_quote']]
     y = df['close']
 
-# Split data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    # Split data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train linear regression model
-    regressor = LinearRegression()
-    regressor.fit(X_train, y_train)
+    # Train model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-    # Make predictions on test set
-    y_pred = regressor.predict(X_test)
+    # Make predictions
+    y_pred = model.predict(X_test)
 
-    # Evaluate model performance
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f"Mean Absolute Error: {mae}")
+    # Evaluate performance
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = sqrt(mse)
 
-    # Make predictions on latest data
-    latest_data = X.tail(1)
-    prediction = regressor.predict(latest_data)
-    return prediction
-
-
+    # Return predictions
+    return y_pred
 # Function to look back at predictions versus actual performance
 def evaluate_performance(data, predictions):
-    # Compare predictions to actual performance
     for symbol in data.keys():
         df = data[symbol]
-        prediction = predictions[symbol]
-        actual_performance = df["close"].tail(1).values[0]
-        error = abs(prediction - actual_performance)
-        print(f"Coin: {symbol}, Prediction: {prediction}, Actual: {actual_performance}, Error: {error}")
+        y_test = df['close'].tail(len(predictions))
+        actual_performance = (y_test.iloc[-1] - y_test.iloc[0]) / y_test.iloc[0] * 100
+        predicted_performance = (predictions[symbol][-1] - predictions[symbol][0]) / predictions[symbol][0] * 100
+        if actual_performance > 1000:
+            print(f"{symbol} actual performance: {actual_performance:.2f}%")
+            print(f"{symbol} predicted performance: {predicted_performance:.2f}%")
+            if actual_performance > predicted_performance:
+                print(f"{symbol} performance was better than predicted.")
+            else:
+                print(f"{symbol} performance was worse than predicted.")
 
 # Main function to run the script
 def main():
-    download_data()
-    data = persist_data()
+    # Download data
+    coins = download_data()
+    # Persist data
+    data = persist_data(coins)
+    # Update data
     data = update_data(data)
+    # Make predictions
     predictions = {}
     for symbol in data.keys():
-        df = data[symbol]
-        predictions[symbol] = predict_price(df)
-    evaluate_performance(data, predictions)
-
-if __name__ == "__main__":
-    main()
+       
